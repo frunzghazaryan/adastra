fd52ceb1b37fae2219f659988be72b6d98fb7869
Frunz Ghazaryan <frunzghazaryan@gmail.com>
2022-10-12 15:34:45 +0200

    feat: add search on home screen

--- src/modules/home/screens/HomeScreen.tsx
+++ src/modules/home/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react'
-import {StyleSheet, FlatList} from 'react-native'
+import React, {useState} from 'react'
+import {StyleSheet, FlatList, TextInput, Text} from 'react-native'
 import {useQuery} from '@tanstack/react-query'
 
 import {HomeScreenProps} from '$src/navigation/types'
@@ -8,6 +8,7 @@ import {getCats} from '$src/api/cats'
 import ListItem from '$src/modules/home/components/ListItem'
 
 const HomeScreen = ({navigation}: HomeScreenProps) => {
+  const [searchStr, setSearchStr] = useState('')
   const {isLoading, data, refetch} = useQuery(['cats'], getCats)
 
   const renderItem = ({item}: {item: CatItem}) => {
@@ -18,16 +19,46 @@ const HomeScreen = ({navigation}: HomeScreenProps) => {
 
   const keyExtractor = (item: CatItem) => item
 
+  const renderSearchResults = () => {
+    if (searchStr.length < 2 || !data) {
+      return null
+    }
+    const _data = data.filter(item =>
+      item.toLowerCase().includes(searchStr.toLowerCase()),
+    )
+    return (
+      <FlatList<CatItem>
+        data={_data}
+        ListEmptyComponent={<Text>No Results...</Text>}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
+        style={styles.searchList}
+        contentContainerStyle={styles.contentContainerStyle}
+      />
+    )
+  }
+
   return (
-    <FlatList<CatItem>
-      data={data}
-      refreshing={isLoading}
-      renderItem={renderItem}
-      keyExtractor={keyExtractor}
-      onRefresh={refetch}
-      style={styles.container}
-      contentContainerStyle={styles.contentContainerStyle}
-    />
+    <>
+      <FlatList<CatItem>
+        data={data}
+        ListHeaderComponent={
+          <TextInput
+            placeholder="Search..."
+            onChangeText={setSearchStr}
+            style={styles.input}
+          />
+        }
+        refreshing={isLoading}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
+        onRefresh={refetch}
+        style={styles.container}
+        contentContainerStyle={styles.contentContainerStyle}
+      />
+
+      {renderSearchResults()}
+    </>
   )
 }
 
@@ -41,5 +72,16 @@ const styles = StyleSheet.create({
   },
   contentContainerStyle: {
     paddingBottom: 40,
+  },
+  searchList: {
+    ...StyleSheet.absoluteFillObject,
+    top: 90,
+    backgroundColor: 'white',
+  },
+  input: {
+    height: 50,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    marginBottom: 20,
   },
 })
